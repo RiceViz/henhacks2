6ee1a22b59d352dee44283cb59eb94b89020843e
Co1etrain <colejm@udel.edu>
2024-03-02 17:03:53 -0500

    yup

--- src/problems.js
+++ src/problems.js
@@ -7,7 +7,7 @@ export default class problems{
         const choose=Math.floor(Math.random() * 2);
         globalNum1=Math.floor(Math.random() * 51);
         globalNum2=Math.floor(Math.random() * 51);
-       
+        if(choose==1){
             const answer= prompt("What is "+globalNum1+"+"+globalNum2)
             const correct=globalNum1+globalNum2
             if(parseInt(answer)==correct){
@@ -16,7 +16,18 @@ export default class problems{
             else {
                 alert("Incorrect! The correct answer is " + correct);
             }
-        
+
+        }
+        else{
+            const answer= prompt("What is "+globalNum1+"-"+globalNum2)
+            const correct=globalNum1-globalNum2
+            if(parseInt(answer)==correct){
+                alert("Correct!");
+            }
+            else {
+                alert("Incorrect! The correct answer is " + correct);
+            }
+        }
         return  globalNum1
       }
     
