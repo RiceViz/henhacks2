e75fa92952aa2dc7b3ecc6af4b99900a2bdf5d3b
CARTER MCCABE <cfmccabe@udel.edu>
2024-03-02 15:36:41 -0500

    players now take turns attacking and defending.

--- src/main.js
+++ src/main.js
@@ -21,71 +21,59 @@ class Player {
         this.name = name;
         this.hp = 100;
         this.selectedMove = null;
+        this.selectedSubAttack = null;
+        this.isDefending = false;
     }
 
     selectMove(move) {
         this.selectedMove = move;
-        this.selectedSubAttack = null; // Reset selected sub-attack when a new move is chosen
     }
 
     selectSubAttack(subAttack) {
         this.selectedSubAttack = subAttack;
     }
 
+    defend() {
+        this.isDefending = true;
+    }
+
     attack(opponent) {
-        let damage = 0;
-        if (this.selectedMove === "punch") {
-            damage = this.calculatePunchDamage();
-        } else if (this.selectedMove === "kick") {
-            damage = this.calculateKickDamage();
+        if (!this.isDefending) {
+            let damage = 0;
+            if (this.selectedMove === "punch") {
+                damage = this.calculatePunchDamage(opponent);
+            } else if (this.selectedMove === "kick") {
+                damage = this.calculateKickDamage(opponent);
+            }
+            opponent.receiveDamage(damage);
         }
-        opponent.receiveDamage(damage);
     }
 
-    calculatePunchDamage() {
-		// if straight
-			// base dmg = 10
-			// give user easy problem(s)
-			// if x block                 <--- refers to enemy's decision
-				// -0.25x effectiveness   <--- hurts attacker
-			// if side block
-				// 0.0x effectiveness
-			// if medial block
-				// 0.25x effectiveness
-			// else               <--- for when both players attack each other (neither player blocks)
-				// 1.0x effectiveness
-
-		// if hook
-			// base dmg = 15
-			// give user medium problem(s)
-			// if x block                 
-				// 0.25x effectiveness   
-			// if side block
-				// -0.25x effectiveness
-			// if medial block
-				// 0.0x effectiveness
-			// else             
-				// 1.0x effectiveness 
-
-		// if uppercut 
-			// base dmg = 20
-			// give user hard problem(s)
-			// if x block                 
-				// 0.25x effectiveness   
-			// if side block
-				// -0.25x effectiveness
-			// if medial block
-				// 0.0x effectiveness
-			// else             
-				// 1.0x effectiveness 
-        return 0;
+    calculatePunchDamage(opponent) {
+        let baseDamage = 0;
+        if (this.selectedSubAttack === "straight") {
+            baseDamage = 10;
+            // Implement sub-attack logic and effectiveness calculation
+        } else if (this.selectedSubAttack === "hook") {
+            baseDamage = 15;
+            // Implement sub-attack logic and effectiveness calculation
+        } else if (this.selectedSubAttack === "uppercut") {
+            baseDamage = 20;
+            // Implement sub-attack logic and effectiveness calculation
+        }
+        return baseDamage;
     }
 
-    calculateKickDamage() {
-        // if axe
-		// if side
-		// if roundhouse
-        return 0;
+    calculateKickDamage(opponent) {
+        let baseDamage = 0;
+        if (this.selectedSubAttack === "axe") {
+            // Implement sub-attack logic and effectiveness calculation
+        } else if (this.selectedSubAttack === "side") {
+            // Implement sub-attack logic and effectiveness calculation
+        } else if (this.selectedSubAttack === "roundhouse") {
+            // Implement sub-attack logic and effectiveness calculation
+        }
+        return baseDamage;
     }
 
     receiveDamage(damage) {
@@ -100,37 +88,30 @@ class Player {
     }
 }
 
-
-export default new Phaser.Game(config)
-
 class Game {
     constructor() {
         this.player1 = new Player("Player 1");
         this.player2 = new Player("Player 2");
     }
-	generateQuestion(difficulty){
-		if (difficulty === 'easy'){
-			//randomly decide either subtraction or addition.
-			//if subtraction:
-				//generate random number (1-100)
-				//generate second number that is (<= first number) and (>= 0).
-				//user must fill in the blank. ex: firstNum=100, secNum=50. user sees "100 - 50 = ___"
-		}
-	}
 
     play() {
         while (this.player1.getHp() > 0 && this.player2.getHp() > 0) {
             console.log(this.player1.name + " HP: " + this.player1.getHp());
             console.log(this.player2.name + " HP: " + this.player2.getHp());
+
+            // Player 1 attacks, Player 2 defends
             this.player1.selectMove(prompt(this.player1.name + "'s turn: Select move (punch/kick/block): "));
-            // After selecting move, ask for sub-attack
-            this.player1.selectSubAttack(prompt("Select sub-attack (uppercut/hook/straight): "));
-
-            this.player2.selectMove(prompt(this.player2.name + "'s turn: Select move (punch/kick/block): "));
-            // After selecting move, ask for sub-attack
-            this.player2.selectSubAttack(prompt("Select sub-attack (uppercut/hook/straight): "));
-
+            this.player1.selectSubAttack(prompt(this.player1.name + "'s turn: Select sub-attack: "));
+            this.player2.defend();
             this.player1.attack(this.player2);
+
+            console.log(this.player1.name + " HP: " + this.player1.getHp());
+            console.log(this.player2.name + " HP: " + this.player2.getHp());
+
+            // Player 2 attacks, Player 1 defends
+            this.player2.selectMove(prompt(this.player2.name + "'s turn: Select move (punch/kick/block): "));
+            this.player2.selectSubAttack(prompt(this.player2.name + "'s turn: Select sub-attack: "));
+            this.player1.defend();
             this.player2.attack(this.player1);
         }
 
